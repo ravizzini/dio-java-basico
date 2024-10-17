e8a804dac107fc07a0ded3162ebe9acb590be6a2
victor.ravizzini <victor.ravizzini@gmail.com>
2024-10-17 17:43:47 -0300

    conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+        
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+
+        Scanner ler = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 237.48;
+
+ 
+        System.out.println("Informe seu nome: \n");
+
+        nomeCliente = ler.nextLine();
+
+        System.out.println("Informe o número da agência: \n");
+
+        agencia = ler.nextLine();
+
+        System.out.println("Informe o número da conta: \n");
+
+        numero = ler.nextInt();
+
+ 
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo +" já está disponível para saque");
+        
+
     }
 }
